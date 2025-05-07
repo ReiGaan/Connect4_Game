f1d9f0cd00124ef184b9754c6ada52dd76f419c8
Anina Morgner <anina.ambra@gmail.com>
2025-05-07 09:55:53 +0200

    implement logic for random agent

--- agents/agent_random/random.py
+++ agents/agent_random/random.py
@@ -1,8 +1,14 @@
 import numpy as np
-from game_utils import BoardPiece, PlayerAction, SavedState
+from game_utils import BoardPiece, PlayerAction, SavedState, check_move_status, MoveStatus
 
 def generate_move_random(
     board: np.ndarray, player: BoardPiece, saved_state: SavedState | None
 ) -> tuple[PlayerAction, SavedState | None]:
-    # Choose a valid, non-full column randomly and return it as `action`
-    return action, saved_state
+    
+    valid_moves = [
+        col for col in range(board.shape[1])
+        if check_move_status(board, PlayerAction(col)) == MoveStatus.IS_VALID
+    ]
+    action = np.random.choice(valid_moves)
+    
+    return PlayerAction(action), saved_state
