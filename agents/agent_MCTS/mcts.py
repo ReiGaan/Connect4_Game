358c786f2730b8b8c549579495f4ca93061ed6e3
Anina Morgner <anina.ambra@gmail.com>
2025-05-07 12:51:08 +0200

    add pseudocode strcuture

--- agents/agent_MCTS/mcts.py
+++ agents/agent_MCTS/mcts.py
@@ -1,8 +1,26 @@
 import numpy as np
 from game_utils import BoardPiece, PlayerAction, SavedState, check_move_status, MoveStatus
+from agent_MCTS.node import node
 
 def mcts_move(
     board: np.ndarray, player: BoardPiece, saved_state: SavedState | None
 ) -> tuple[PlayerAction, SavedState | None]:
-    # Choose a valid, non-full column with MCTS and return it as `action`
+    for i in range(iterationnumber):
+        node = board
+        # Selection
+        while node is fully_expanded and not terminal:
+            node = select_child_with_highest_UCT(node)
+
+        # Expansion
+        if node is not terminal:
+            new_child = expand_random_unvisited_child(node)
+            simulation_result = simulate_random_game(new_child)
+            
+            # Backpropagation
+            backpropagate(new_child, simulation_result)
+        else:
+            # If terminal, simulate from here (though sometimes this step is skipped)
+            result = game_result(node)
+            backpropagate(node, result)
+
     return action, saved_state
